fe9733087e8e7014b90d0b25a6bcd0c1f080cc6c
Emino_wp61@itstep.edu.az <Emino_wp61@itstep.edu.az>
2023-12-01 15:51:52 +0400

    Add project files.

---
+++ TakeScreenShot WPF UDP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TakeScreenShot_WPF_UDP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
